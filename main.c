0d6b0fe8dc015ab8e39d300132b5b4909140d84d
Mike Looijmans <mike.looijmans@topic.nl>
2014-01-06 13:46:18 +0100

    Add GPLv3 license text to all source files

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 /*
- * Copyright (C) 2013 Topic Embedded Systems
- * 
- * This program is free software: you can redistribute it and/or modify
+ * main.c
+ *
+ * Dyplo loadable kernel module.
+ *
+ * (C) Copyright 2013,2014 Topic Embedded Products B.V. <Mike Looijmans> (http://www.topic.nl).
+ * All rights reserved.
+ *
+ * This file is part of kernel-module-dyplo.
+ * kernel-module-dyplo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * kernel-module-dyplo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with <product name>.  If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA or see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact Topic by electronic mail via info@topic.nl or via
+ * paper mail at the following address: Postbus 440, 5680 AK Best, The Netherlands.
  */
-// #define DEBUG
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/init.h>

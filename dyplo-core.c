topic-embedded-products/kernel-module-dyplo

commit c0f3f448a8624cdaa6abd45a3204be719b86f4c3

Add IOctl function to remove single route

dyplo-core.c
@@ -421,6 +421,7 @@ static int dyplo_ctl_route_add(struct dyplo_dev *dev, struct dyplo_route_item_t 
 	return 0;
 }
 
+
 static int dyplo_ctl_route_add_from_user(struct dyplo_dev *dev, const struct dyplo_route_t __user *uroutes)
 {
 	int status = 0;
@@ -521,6 +522,25 @@ static int dyplo_ctl_route_delete(struct dyplo_dev *dev, int ctl_index_to_delete
 	}
 	return 0;
 }
+
+
+static int dyplo_ctl_route_single_delete(struct dyplo_dev *dev, struct dyplo_route_item_t route)
+{
+	u32 dst_route;
+
+	pr_debug("%s %d,%d->%d,%d\n", __func__,
+		route.srcNode, route.srcFifo, route.dstNode, route.dstFifo);
+	if ((route.srcNode >= dev->number_of_config_devices) ||
+	    (route.dstNode >= dev->number_of_config_devices))
+	{
+		pr_debug("%s: Invalid source or destination\n", __func__);
+	    return -EINVAL;
+	}
+	dst_route = ((route.dstNode + 1) << DYPLO_STREAM_ID_WIDTH) | route.dstFifo;
+	dyplo_ctl_route_remove_dst(dev, dst_route);
+	return 0;
+}
+
 
 static int dyplo_ctl_route_clear(struct dyplo_dev *dev)
 {
@@ -638,6 +658,13 @@ static long dyplo_ctl_ioctl_impl(struct dyplo_dev *dev, unsigned int cmd, unsign
 		case DYPLO_IOC_ROUTE_DELETE: /* Remove routes to a node */
 			status = dyplo_ctl_route_delete(dev, arg);
 			break;
+        case DYPLO_IOC_ROUTE_SINGLE_DELETE: /* Remove single route */
+        {
+            union dyplo_route_item_u u;
+			u.route = arg;
+			status = dyplo_ctl_route_single_delete(dev, u.route_item);
+			break;
+        }
 		case DYPLO_IOC_BACKPLANE_STATUS:
 			status = dyplo_reg_read_quick(dev->base, DYPLO_REG_BACKPLANE_ENABLE_STATUS) >> 1;
 			break;

dyplo-ioctl.h
@@ -127,6 +127,8 @@ struct dyplo_dma_configuration_req {
 #define DYPLO_IOC_LICENSE_KEY	0x30
 #define DYPLO_IOC_STATIC_ID	0x31
 
+#define DYPLO_IOC_ROUTE_SINGLE_DELETE 0x32
+
 /* S means "Set" through a ptr,
  * T means "Tell", sets directly
  * G means "Get" through a ptr
@@ -142,6 +144,8 @@ struct dyplo_dma_configuration_req {
 #define DYPLO_IOCTROUTE   _IO(DYPLO_IOC_MAGIC, DYPLO_IOC_ROUTE_TELL)
 /* Remove routes to a node. Argument is a integer node number. */
 #define DYPLO_IOCTROUTE_DELETE   _IO(DYPLO_IOC_MAGIC, DYPLO_IOC_ROUTE_DELETE)
+/* Remove single route. Argument is a dyplo_route_item_t cast to integer */
+#define DYPLO_IOCTROUTE_SINGLE_DELETE   _IO(DYPLO_IOC_MAGIC, DYPLO_IOC_ROUTE_SINGLE_DELETE)
 
 /* Add a route from "this" dma or cpu node to another node. The argument
  * is an integer of destination node | fifo << 8 */

commit bdce9bec78ccabb908d307fc5025a23857c59e4a

Cosmetic: Fix indentation whitespace
Fixes: c0f3f448a862 "Add IOctl function to remove single route"

dyplo-core.c
@@ -664,13 +664,13 @@ static long dyplo_ctl_ioctl_impl(struct dyplo_dev *dev, unsigned int cmd, unsign
 		case DYPLO_IOC_ROUTE_DELETE: /* Remove routes to a node */
 			status = dyplo_ctl_route_delete(dev, arg);
 			break;
-        case DYPLO_IOC_ROUTE_SINGLE_DELETE: /* Remove single route */
-        {
-            union dyplo_route_item_u u;
+		case DYPLO_IOC_ROUTE_SINGLE_DELETE: /* Remove single route */
+		{
+			union dyplo_route_item_u u;
 			u.route = arg;
 			status = dyplo_ctl_route_single_delete(dev, u.route_item);
 			break;
-        }
+		}
 		case DYPLO_IOC_BACKPLANE_STATUS:
 			status = dyplo_reg_read_quick(dev->base, DYPLO_REG_BACKPLANE_ENABLE_STATUS) >> 1;
 			break;